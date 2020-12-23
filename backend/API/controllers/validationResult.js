60f02d439f6bf0a532bf6be9ccae5a8d9c31124e
Ali Azlan <aliazlan2002@gmail.com>
2020-12-23 23:23:22 +0500

    validationresult controller

---
+++ backend/API/controllers/validationResult.js
@@ -0,0 +1,84 @@
+const { validationResult } = require('express-validator')
+
+module.exports = {
+    adminSignup: function(req, res, next){
+        const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+        if(!errors.isEmpty()){
+            const errlist = errors.array().map(error => error.msg)
+            req.flash('error', errlist)
+            return res.redirect('register')
+        }
+        next()
+    },
+
+    //doctor is registered by admin so the use of this function is in 
+    doctorSignup: function(req, res ,next){
+        const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+        if(!errors.isEmpty()){
+            const errlist = errors.array().map(error => error.msg)
+            req.flash('error', errlist)
+            return res.redirect('addnewdoctor')
+        }
+        next()
+    },
+
+    patientSignup: function(req, res, next){
+        const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy .log("here ")
+        if(!errors.isEmpty()){
+            const errlist = errors.array().map(error => error.msg)
+            req.flash('error', errlist)
+            return res.redirect('register')
+        }
+        next()
+    },
+
+    adminEdit: function(req, res, next){
+        const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+        if(!errors.isEmpty()){
+            const errlist = errors.array().map(error => error.msg)
+            req.flash('error', errlist)
+            return res.redirect('profile')
+        }
+        next()
+    },
+
+    doctorEdit: function(req, res ,next){
+        const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+        if(!errors.isEmpty()){
+            const errlist = errors.array().map(error => error.msg)
+            req.flash('error', errlist)
+            return res.redirect('profile')
+        }
+        next()
+    },
+
+    patientEdit: function(req, res, next){
+        const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy .log("here ")
+        if(!errors.isEmpty()){
+            const errlist = errors.array().map(error => error.msg)
+            req.flash('error', errlist)
+            return res.redirect('profile')
+        }
+        next()
+    },
+
+    adminDoctorEdit: function(req, res, next){
+        const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+        if(!errors.isEmpty()){
+            const errlist = errors.array().map(error => error.msg)
+            req.flash('error', errlist)
+            return res.redirect('showalldoctors')
+        }
+        next()
+    },
+
+    emailOrPasswordChange: function(req, res, next){
+        const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+        if(!errors.isEmpty()){
+            const errlist = errors.array().map(error => error.msg)
+            req.flash('error', errlist)
+            return res.redirect('dashboard')
+        }
+        next()
+    },
+}
