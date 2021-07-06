b92f32da80058dc91a4695b97220c318b05c3325
Omar-ALkhateeb <omar.alkhateeb9919@gmail.com>
2021-07-06 13:13:27 -0800

    fixed nodemailer

--- backend/API/functions/sendmail.js
+++ backend/API/functions/sendmail.js
@@ -1,13 +1,14 @@
 const nodemailer = require('nodemailer')
 
 module.exports = {
-    sendmail: function (receiver, subject, message) {
+    sendmail: async function (receiver, subject, message) {
+        // console.log(process.env.EMAIL)
         // Step 1=
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: process.env.EMAIL || 'servicegolang078@gmail.com',
-                pass: process.env.GMAIL_PASSWORD || '1234566777oo'
+                user: process.env.EMAIL || 'example@gmail.com',
+                pass: process.env.GMAIL_PASSWORD || '1234'
             }
         });
 
@@ -23,11 +24,10 @@ module.exports = {
         };
 
         // Step 3
-        transporter.sendMail(mailOptions, (error, data) => {
-            console.log(data)
-            if (error) {
-                return error
-            }
-        });
+        let info = await transporter.sendMail(mailOptions);
+
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
     }
 }
